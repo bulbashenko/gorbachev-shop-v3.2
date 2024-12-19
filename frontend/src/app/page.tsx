07ca308e105e9c4756a5481be0bb47b738b8af7b
bulbashenko <bulbashenko69@gmail.com>
2024-12-19 17:46:59 +0100

    Removed useCurrency hook from page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,19 +2,14 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { useSelector } from 'react-redux';
-import { useCurrency } from '@/hooks/useCurrency';
 
 const HomePage: React.FC = () => {
   const t = useTranslations();
-  useCurrency();
-  const currency = useSelector((state: RootState) => state.currency.currency);
   
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className='font-bold text-4xl'>{t("hello")}</h1>
       {t("welcome")}
-      {currency}
     </div>
   );
 };
